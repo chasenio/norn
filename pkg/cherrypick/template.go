7f8acdf8f9b53d449302441e19f14a9f2340bf50
chasenio <13550898+chasenio@users.noreply.github.com>
2025-11-13 21:55:37 +0800

    refactor: refactor cherry-pick service

--- pkg/cherrypick/template.go
+++ pkg/cherrypick/template.go
@@ -2,40 +2,26 @@ package cherrypick
 
 import tp "github.com/chasenio/norn/pkg/types"
 
+type MessageTemplate struct {
+	UniqueID string
+	Text     string
+}
+
 // Templates holds the template configurations for cherry-pick comments.
 type Templates struct {
-	// SummaryTemplate is the template for generating cherry-pick summary comments.
-	// It should contain a {{ .Message }} placeholder for the branch list.
-	SummaryTemplate string
-	
-	// ResultTemplate is the template for generating cherry-pick result comments.
-	// It should contain a {{ .Message }} placeholder for the result table.
-	ResultTemplate string
+	Summary          *MessageTemplate
+	CherryPickResult *MessageTemplate
 }
 
 // DefaultTemplates returns the default templates for cherry-pick operations.
 func DefaultTemplates() *Templates {
 	return &Templates{
-		SummaryTemplate: tp.CherryPickTaskSummaryTemplate,
-		ResultTemplate:  tp.PickResultTemplate,
+		Summary: &MessageTemplate{
+			UniqueID: tp.CherryPickSummaryFlag,
+			Text:     tp.CherryPickTaskSummaryTemplate},
+		CherryPickResult: &MessageTemplate{
+			UniqueID: tp.CherryPickResultFlag,
+			Text:     tp.CherryPickResultTemplate,
+		},
 	}
-}
-
-// NewTemplates creates a new Templates with the provided values.
-// If any template is empty, it will use the default template.
-func NewTemplates(summaryTemplate, resultTemplate string) *Templates {
-	templates := &Templates{
-		SummaryTemplate: summaryTemplate,
-		ResultTemplate:  resultTemplate,
-	}
-	
-	// Use defaults if not provided
-	if templates.SummaryTemplate == "" {
-		templates.SummaryTemplate = tp.CherryPickTaskSummaryTemplate
-	}
-	if templates.ResultTemplate == "" {
-		templates.ResultTemplate = tp.PickResultTemplate
-	}
-	
-	return templates
 }
